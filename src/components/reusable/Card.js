bd1af1a3c82ba320f9ba1c7ebf6bcc1328b18ca8
Raksmeysomaly <raksmeysomalyek0076@gmail.com>
2024-09-09 15:31:09 +0700

    ft#7-card: added the props and declare social media icons

--- src/components/reusable/Card.js
+++ src/components/reusable/Card.js
@@ -8,3 +8,32 @@ import MiniMap from '../../assets/svg/miniMap.svg'
 import MiniClock from '../../assets/svg/miniClock.svg';
 import Calender from '../../assets/svg/calender.svg';
 // import NODE_ENV
+
+const Card = ({
+    image,
+    imageAlt,
+    title,
+    description,
+    facebookLink,
+    instagramLink,
+    twitterLink,
+    youtubeLink,
+    websiteLink,
+    location,
+    deadLine,
+    timeOut
+}) => {
+    const socialMediaIcons = [
+        { icon: Facebook, linkKey: facebookLink },
+        { icon: Twitter, linkKey: twitterLink },
+        { icon: Instagram, linkKey: instagramLink },
+        { icon: Youtube, linkKey: youtubeLink },
+        { icon: Chrome, linkKey: websiteLink, isExternal: true },
+    ];
+    const companyUrl = title.toLowerCase().replace(/\s+/g, '-');
+    const workDetails = [
+        { icon: MiniClock, linkKey: timeOut },
+        { icon: Calender, linkKey: deadLine },
+        { icon: MiniMap, linkKey: location },
+    ]
+}
