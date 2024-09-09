c5eaa3b989279109d8c8949f1e5cfe42211f375e
Raksmeysomaly <raksmeysomalyek0076@gmail.com>
2024-09-09 15:21:26 +0700

    ft#7-card: imported necessary icons

---
+++ src/components/reusable/Card.js
@@ -0,0 +1,10 @@
+import { Facebook, Instagram, Twitter, Youtube, Chrome } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { cardBody, cardTitle, cardDescription } from './tailwindcardclass/cardClass';
+import Map from '../../assets/svg/map.svg';
+import BookMark from '../../assets/svg/bookmark.svg';
+import Button from './ButtonComponent';
+import MiniMap from '../../assets/svg/miniMap.svg'
+import MiniClock from '../../assets/svg/miniClock.svg';
+import Calender from '../../assets/svg/calender.svg';
+// import NODE_ENV
