0ee5f1cad5b9821a3cd0fe7d2b59835c748b38dc
Sambath Prak <sambathprak789@gmail.com>
2024-09-12 12:57:22 +0700

    Feature#6-filter: fixed overwrite class name with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
+    extend: {
+      fontFamily: {
+        primary: ['Poppins']
+      },
+      colors: {
       "light-100": "#375761",
       "light-300": "#808A8D",
       "light-500": "#ADB6B9",
       "light-700": "#ECEEEF",
       "light-900": "#FBFBFB",
       primary: "#7CD1EB"
-    },
-    extend: {
-      fontFamily: {
-        primary: ['Poppins']
-      }
+    }
     },
   },
   plugins: [],
