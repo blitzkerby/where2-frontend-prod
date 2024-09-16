16e0e3e05d43d853fe6cce5bf564ec3031060778
AnB School 0016 <anbschool0016@AnBs-Mac-mini-4.local>
2024-09-16 15:38:52 +0700

    ft2-footer : created and add footer to the HomePage file

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import Footer from "../components/reusable/footerComponent/Footer";
+const HomePage = () => {
+  return (
+    <div>
+
+      <Footer/>
+    </div>
+  );
+};
+
+export default HomePage;
