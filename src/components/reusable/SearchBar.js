f001f2f461b12b989e8d4b360addfc0d0af15983
Yun Chankrisna <chankrisnayun@gmail.com>
2024-09-17 13:25:34 +0700

    testing review board

--- src/components/reusable/SearchBar.js
+++ src/components/reusable/SearchBar.js
@@ -12,7 +12,7 @@ function SearchBar({ handleSearch, searchPlaceholder }) {
                 className="absolute right-3 top-1/2 transform -translate-y-1/2"
                 aria-label="Search"
             >
-                {/* <Search className="h-5 w-5 text-gray-400 z-[0]" /> */}
+                <Search className="h-5 w-5 text-gray-400 z-[0]" />
             </button>
         </form>
 )
