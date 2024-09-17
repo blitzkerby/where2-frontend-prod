657f481648fc34e285057ee01916023ef67b9756
Sambath Prak <sambathprak789@gmail.com>
2024-09-17 11:30:53 +0700

    feature/Feature#9-accommodation: initial accommodation page

---
+++ src/pages/AccommodationPage.js
@@ -0,0 +1,10 @@
+import AccommodationsList from "../components/AccommodationsList";
+
+function AccommodationPage() {
+  return <div>
+  <div>Accommodation Page</div>
+  <AccommodationsList />
+  </div>
+}
+
+export default AccommodationPage;
