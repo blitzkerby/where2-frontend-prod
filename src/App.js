fa8da5f6e1b3c133193cca569dc38ddc452a864d
Yun Chankrisna <chankrisnayun@gmail.com>
2024-09-19 09:30:00 +0700

    User Profile modification & Side bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Card from "./components/reusable/Card";
 import SearchBar from "./components/reusable/SearchBar";
 import HomePage from "./pages/HomePage";
+<<<<<<< HEAD
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgetPasswordPage from "./pages/ForgetPasswordPage";
@@ -10,6 +11,9 @@ import TermsAndConditionsPage from "./pages/TermsAndConditionsPage";
 import VerificationPage from "./pages/VerificationPage";
 import JobPage from "./pages/JobPage";
 import JobDetailPage from "./pages/JobDetailPage";
+=======
+import Profile from "./components/reusable/Profile";
+>>>>>>> aafd85a (User Profile modification & Side bar)
 import UserProfile from "./components/UserProfile";
 
 
@@ -23,6 +27,7 @@ import UserProfile from "./components/UserProfile";
       path:"/home",
       element:<HomePage/>
     },
+<<<<<<< HEAD
     { path: "/login",element: <LoginPage />},
     { path: "/signup",element: <RegisterPage />},
     { path: "/forget-password", element: <ForgetPasswordPage />},
@@ -33,6 +38,13 @@ import UserProfile from "./components/UserProfile";
     { path: "/job", element: <JobPage /> },
     { path: "/job-detail/:id", element: <JobDetailPage />},
     
+=======
+    {
+      path:'/profile',
+      element:<UserProfile/>
+    },
+
+>>>>>>> aafd85a (User Profile modification & Side bar)
   ])
   
 function App() {
