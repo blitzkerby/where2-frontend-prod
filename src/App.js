aafd85a5b1385f7cdd166d4900dd374e64631964
Yun Chankrisna <chankrisnayun@gmail.com>
2024-09-19 09:30:00 +0700

    User Profile modification & Side bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import Card from "./components/reusable/Card";
 // import SearchBar from "./components/reusable/SearchBar";
 import HomePage from "./pages/HomePage";
+import Profile from "./components/reusable/Profile";
+import UserProfile from "./components/UserProfile";
 
 
   const router = createBrowserRouter([
@@ -13,7 +15,12 @@ import HomePage from "./pages/HomePage";
     {
       path:"/home",
       element:<HomePage/>
-    }
+    },
+    {
+      path:'/profile',
+      element:<UserProfile/>
+    },
+
   ])
   
 function App() {
