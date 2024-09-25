6a4b84f39b899e05ef190b80e668a77eea79baf4
Raksmeysomaly <raksmeysomalyek0076@gmail.com>
2024-09-25 16:53:22 +0700

    ft#7.1-job: create ContactCard and DetailText component

--- src/components/job/JobDetail.js
+++ src/components/job/JobDetail.js
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchCompany } from "../../features/slices/jobSlice";
+import ContactCard from "../reusable/ContactCard";
+import DetailText from "../reusable/DetailText";
 const JobDetail = () => {
     const dispatch = useDispatch();
     const {company } = useSelector(state => state.job);
@@ -9,14 +11,21 @@ const JobDetail = () => {
         dispatch(fetchCompany(7));
 
     }, [])
-    console.log("this is company with job 7", company);
+    // console.log("this is company with job 7", company);
     // company.map(x => console.log(x.company.company_bg))
 
     
     return (
-        <div>
-            this is detail page
-        </div>
+        <>
+            <div className={`lg:w-[676px] mt-[78px] flex-col mx-auto text-center `}>
+                <h1 className="pb-6"><b>Amazon Cafe</b></h1>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB6qQYTiaG58zHg3LwPcbPaqOrkFmAschW8A&s" className="w-40 h-40 rounded-full mx-auto"/>
+            </div>
+            <DetailText title={"Company Information1"} content={"This is Company Content1"}/>
+            <DetailText title={"Company Information"} content={"This is Company Content"}/>
+            <ContactCard title={"Contact1"} content={"Content hgere1"}/>
+            <ContactCard title={"Contact"} content={"Content hgere"}/>
+        </>
     )
 };
 
