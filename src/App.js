f091e64369d258ee7893276c59b9fda140273d06
Admin-Kerby <hakkerby01@gmail.com>
2024-10-04 19:10:36 +0700

    feature#3.1.1-University: COMMENTED OUT QUERYCLIENT IMPORT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
