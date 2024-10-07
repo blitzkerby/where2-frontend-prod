64b08e97cf4caa8b4dae240972396240263c7a7d
Sovattana Sochit <sdasdaw1@gmail.com>
2024-10-07 10:09:34 +0700

    uptodate

---
+++ src/components/PartTimeJobListing.js
@@ -0,0 +1,50 @@
+import ListingComponent from "./reusable/ListingComponent";
+
+const PartTimeJobListing = () => {
+  const jobs = [
+    {
+      id: 1,
+      title: "Cleaner",
+      company : "KFC",
+      location : "Wat Phnom",
+      salary:"200$",
+    },
+    {
+      id: 2,
+      title: "Cleaner",
+      company : "KFC",
+      location : "Wat Phnom",
+      salary:"200$",
+    },
+    {
+      id: 3,
+      title: "Cleaner",
+      company : "KFC",
+      location : "Wat Phnom",
+      salary:"200$",
+    },
+    {
+      id: 4,
+      title: "Cleaner",
+      company : "KFC",
+      location : "Wat Phnom",
+      salary:"200$",
+    },
+    {
+      id: 5,
+      title: "Cleaner",
+      company : "KFC",
+      location : "Wat Phnom",
+      salary:"200$",
+    },
+  ];
+  return (
+    <ListingComponent
+      title="PART-TIME JOBS"
+      data={jobs}
+      columns={["Title", "Company", "Location", "Salary"]}
+      totalItems={jobs.length}
+    />
+  );
+};
+export default PartTimeJobListing
