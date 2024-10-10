9b4658a66edfcabfc5bf52abce4a0069293ffee7
SamedyHUNX <samedy.hunx@gmail.com>
2024-10-10 17:09:07 +0700

    feature/#5.1.1-hosting: adding hero of the community pagre

--- src/components/DiscussionsComponent.js
+++ src/components/DiscussionsComponent.js
@@ -3,6 +3,25 @@ import CreateDiscussionComponent from './../components/reusable/CreateDiscussion
 import DiscussionList from './../components/reusable/DiscussionList';
 import useAuth from './../hooks/useAuth';
 
+const CommunityHeroSection = () => {
+  return (
+    <div className='w-full bg-black h-[600px] sm:h-[45vh] text-white'>
+      <div className='my-[96px] w-full h-full flex'>
+        <div>
+          <h1>Dive into Anything</h1>
+          <p>
+            W2COMMUNITY is home to thousands of communities, students, and researchers from all around the world.
+            Join us in contributing to the community, whether you're a student, a teacher, or a writer.
+          </p>
+        </div>
+        <div>
+          <img src='https://w2community.com/img/hero.png' alt="W2 LOGO"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const DiscussionsComponent = () => {
   const [showCreateForm, setShowCreateForm] = useState(false);
   const { isLoggedIn, role } = useAuth();
@@ -13,7 +32,9 @@ const DiscussionsComponent = () => {
   };
 
   return (
-    <div className="container mt-[64px] min-h-screen mx-auto py-8">
+    <div>
+    <CommunityHeroSection/>
+    <div className="container min-h-screen mx-auto py-8">
       {showCreateForm && isLoggedIn && allowedRoles.includes(role) ? (
         <CreateDiscussionComponent 
           onDiscussionCreated={handleDiscussionCreated} 
@@ -23,6 +44,7 @@ const DiscussionsComponent = () => {
           onNewDiscussionClick={() => setShowCreateForm(true)} 
         />
       )}
+      </div>
     </div>
   );
 };
