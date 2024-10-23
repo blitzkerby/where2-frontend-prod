591ac5449f866e2232a5b3f8d1c42e1ef9e7d951
SamedyHUNX <samedy.hunx@gmail.com>
2024-10-23 10:09:49 +0700

    bugfix/bugfix#5.1-authentication-and-debugging: more bug fixes on home page

--- src/components/reusable/JoinUs.js
+++ src/components/reusable/JoinUs.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const joinUs = () => {
 	return (
-		<div className="relative w-full h-[550px] pb-[64px]">
+		<div className="relative w-full lg:h-[925px] h-[550px] mt-[32px] pb-[64px]">
 			<img src={KeepInWithUs} className="absolute top-0 left-0 w-full h-full object-cover" />
 			<div className="absolute inset-0 bg-black opacity-25"></div>
 			<div className="absolute inset-0 flex flex-col items-center justify-center">
@@ -12,7 +12,7 @@ const joinUs = () => {
 					KEEP IN TOUCH WITH US
 				</h1>
 				<Link to="/signup">
-					<button className="text-[#375761] bg-white rounded-[100px] p-2 px-5 drop-shadow-lg" >
+					<button className="text-[#375761] bg-white rounded-[100px] p-2 px-5 drop-shadow-lg hover:scale-105" >
 						Sign Up Now!
 					</button>
 				</Link>
