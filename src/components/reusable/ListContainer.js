3b4dfa9452c3e19f33d5880c534b2179c102a757
Raksmeysomaly <raksmeysomalyek0076@gmail.com>
2024-10-25 11:44:40 +0700

    deleted max-w-full in card component

--- src/components/reusable/ListContainer.js
+++ src/components/reusable/ListContainer.js
@@ -1,6 +1,6 @@
 const ListContainer = ({children}) => {
     return (
-        <div className="max-w-full min-h-screen mx-auto grid 
+        <div className="min-h-screen mx-auto grid 
         max-w-[1200px] lg:mt-[248px]
         sm:w-[100%] sm:px-[35px] sm:mt-[6.9rem]
         gap-[30px] 
